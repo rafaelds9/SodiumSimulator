2284e726712ec6f4de9b8dcca3839b3966264f96
rafaelds9 <duarte.rafael@academico.ifpb.edu.br>
2019-07-15 14:22:08 -0300

    Heatmap is updated

--- plot_test.py
+++ plot_test.py
@@ -5,27 +5,36 @@ import matplotlib.pyplot as plt
 DIM_X = 5 #Pegar com base no header
 DIM_Y = 3
 
-selected_line = 188 #depois posso criar um iterador
 
 data = pd.read_csv("data.txt")
 
 #print(data.iloc[selected_line:selected_line+1,:].values[0])
 
-npdata = np.array([])
 
-for i in range(0, DIM_X):
-    for j in range(0, DIM_Y):
-        npdata = np.append(npdata,data.iloc[selected_line:selected_line+1,DIM_Y*i+j].values[0])
+for selected_line in range(0, data.shape[0]):
 
-npdata = npdata.reshape(DIM_X,DIM_Y)
-print(npdata)
+    npdata = np.array([])
+    for i in range(0, DIM_X):
+        for j in range(0, DIM_Y):
+            npdata = np.append(npdata,data.iloc[selected_line:selected_line+1,DIM_Y*i+j].values[0])
+
+    npdata = npdata.reshape(DIM_X,DIM_Y)
+    print(npdata)
 
 
-y = np.arange(0,DIM_Y)
-plt.xticks(y)
-plot = plt.imshow(npdata, cmap='viridis', vmin=0,vmax=0.5)
-plt.colorbar(plot)
-plt.tight_layout()
+    if selected_line == 0:
+        plt.title("Diffusion")
+        p = plt.imshow(npdata)
+        plt.colorbar(p)
+        plt.show()
+        p = plt.imshow(npdata)
+        plt.colorbar(p)
+        fig = plt.gcf()
+        plt.clim()   # clamp the color limits
+        plt.title("Diffusion")
+    else:
+        p.set_data(npdata)
+
+    print("step", selected_line)
+    plt.pause(0.1)
 plt.show()
-
-#plotar uma animacao do heatmap se atualizando
